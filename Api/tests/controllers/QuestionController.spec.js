24c34b47fc3de2080160420f0e42201281d061e7
Eric Nograles <grales@gmail.com>
2015-01-26 05:59:16 -0500

    Unit testing!

---
+++ Api/tests/controllers/QuestionController.spec.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+
+describe('QuestionController', function() {
+  it('should pass a basic asynchronous unit test', function(done) {
+    assert(true);
+    done();
+  });
+});
